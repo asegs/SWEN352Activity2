f321d2e338b3ca982df92910a31738cc83c1416b
Nikhil Raina <nikhilrainathegreat@gmail.com>
2021-03-07 11:53:09 -0500

    Added readable structure for future use and recollection

--- tests/test_library.py
+++ tests/test_library.py
@@ -5,62 +5,215 @@ from unittest.mock import Mock
 class TestLibrary(TestCase):
     import library.library as library
 
+    def test_constructor_works_as_intended(self):
+        # Assume
+
+
+        # Action
+
+
+        # Assert
+
+        pass
+
     def test_is_ebook(self):
-        self.fail()
+        # Assume
+
+
+        # Action
+
+
+        # Assert
+
+        pass
 
     def test_book_is_ebook(self):
+        # Assume
+
+
+        # Action
+
+
+        # Assert
+
         pass
 
     def test_book_is_not_ebook(self):
+        # Assume
+
+
+        # Action
+
+
+        # Assert
         pass
 
     def test_gets_book_count_for_more_than_zero(self):
+        # Assume
+
+
+        # Action
+
+
+        # Assert
         pass
 
     def test_gets_book_count_for_zero_books(self):
+        # Assume
+
+
+        # Action
+
+
+        # Assert
         pass
 
     def test_gets_book_count_if_no_book_is_there(self):
+        # Assume
+
+
+        # Action
+
+
+        # Assert
         pass
 
     def test_author_entered_for_book_is_true(self):
+        # Assume
+
+
+        # Action
+
+
+        # Assert
         pass
 
     def test_author_entered_for_book_is_false(self):
+        # Assume
+
+
+        # Action
+
+
+        # Assert
         pass
 
     def test_gets_languages_for_the_book(self):
+        # Assume
+
+
+        # Action
+
+
+        # Assert
         pass
 
     def test_gets_one_language_for_the_book(self):
+        # Assume
+
+
+        # Action
+
+
+        # Assert
         pass
 
     def test_gets_zero_language_for_no_book(self):
+        # Assume
+
+
+        # Action
+
+
+        # Assert
         pass
 
     def test_gets_the_correct_languages_for_the_book(self):
+        # Assume
+
+
+        # Action
+
+
+        # Assert
         pass
 
     def test_patron_added_to_library_db(self):
+        # Assume
+
+
+        # Action
+
+
+        # Assert
         pass
 
     def test_patron_exists_in_library_db(self):
+        # Assume
+
+
+        # Action
+
+
+        # Assert
         pass
 
     def test_patron_is_registered_in_db(self):
+        # Assume
+
+
+        # Action
+
+
+        # Assert
         pass
 
     def test_patron_is_not_registered_in_db(self):
+        # Assume
+
+
+        # Action
+
+
+        # Assert
         pass
 
     def test_patron_has_borrowed_book(self):
+        # Assume
+
+
+        # Action
+
+
+        # Assert
         pass
 
     def test_has_patron_borrowed_book_true(self):
+        # Assume
+
+
+        # Action
+
+
+        # Assert
         pass
 
     def test_has_patron_borrowed_book_false(self):
+        # Assume
+
+
+        # Action
+
+
+        # Assert
         pass
 
     def test_patron_has_returned_book(self):
+        # Assume
+
+
+        # Action
+
+
+        # Assert
         pass
