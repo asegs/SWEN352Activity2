256a5a95b8ed1236d918f94564f1d9ee1f310c62
Nikhil Raina <nikhilrainathegreat@gmail.com>
2021-03-07 01:49:07 -0500

    Added the headings of all the tests needed for the Library.py

---
+++ tests/test_library.py
@@ -0,0 +1,78 @@
+import unittest
+
+import library.library as library
+
+def test_book_is_ebook():
+    pass
+
+
+def test_book_is_not_ebook():
+    pass
+
+
+def test_gets_book_count_for_more_than_zero():
+    pass
+
+
+def test_gets_book_count_for_zero_books():
+    pass
+
+
+def test_gets_book_count_if_no_book_is_there():
+    pass
+
+
+def test_author_entered_for_book_is_true():
+    pass
+
+
+def test_author_entered_for_book_is_false():
+    pass
+
+
+def test_gets_languages_for_the_book():
+    pass
+
+
+def test_gets_one_language_for_the_book():
+    pass
+
+
+def test_gets_zero_language_for_no_book():
+    pass
+
+
+def test_gets_the_correct_languages_for_the_book():
+    pass
+
+
+def test_patron_added_to_library_db():
+    pass
+
+
+def test_patron_exists_in_library_db():
+    pass
+
+
+def test_patron_is_registered_in_db():
+    pass
+
+
+def test_patron_is_not_registered_in_db():
+    pass
+
+
+def test_patron_has_borrowed_book():
+    pass
+
+
+def test_has_patron_borrowed_book_true():
+    pass
+
+
+def test_has_patron_borrowed_book_false():
+    pass
+
+
+def test_patron_has_returned_book():
+    pass
