b0e1ddeec1ba86ee305ff057599757baed3c8115
cmr4494 <cmr4494@rit.edu>
2021-03-09 13:05:22 -0500

    97% for library_library_db_interface_py.html

--- tests/test_library_db_interface.py
+++ tests/test_library_db_interface.py
@@ -20,7 +20,7 @@ class TestLibraryDBInterface(unittest.TestCase):
         CuT.close_db()
         CuT.db = Mock()
         CuT.db.search = Mock()
-        CuT.db.search.return_value = None #patron is not in db
+        CuT.db.search.return_value = None  # patron is not in db
         CuT.db.insert = Mock()
         CuT.db.insert.return_value = 1
 
@@ -42,9 +42,82 @@ class TestLibraryDBInterface(unittest.TestCase):
         CuT.db.insert.return_value = 1
 
         CuT.retrieve_patron = Mock()
-        CuT.retrieve_patron.return_value = mock_patron #Can't subscript a mock so do this for now
-        #and test retrieve_patron method later
+        CuT.retrieve_patron.return_value = mock_patron  # Can't subscript a mock so do this for now
+        # and test retrieve_patron method later
 
         CuT.insert_patron(mock_patron)
 
         CuT.db.insert.assert_not_called()  # patron is not inserted because patron is in db
+
+    def test_get_patron_count(self):
+        CuT = Library_DB()
+        CuT.close_db()
+        CuT.db = Mock()
+        CuT.db.all = Mock()
+        CuT.db.all.return_value = []  # list of patrons in db
+        self.assertEqual(0, CuT.get_patron_count())
+
+    def test_get_patron_count_multiple(self):
+        mock_patron = Mock(Patron)
+        mock_patron.get_memberid = Mock()
+
+        CuT = Library_DB()
+        CuT.close_db()
+        CuT.db = Mock()
+        CuT.db.all = Mock()
+        CuT.db.all.return_value = [mock_patron, mock_patron]  # list of patrons in db
+        self.assertEqual(2, CuT.get_patron_count())
+
+    def test_get_all_patrons(self):
+        CuT = Library_DB()
+        CuT.close_db()
+        CuT.db = Mock()
+        CuT.db.all = Mock()
+        CuT.db.all.return_value = []  # list of patrons in db
+        self.assertEqual([], CuT.get_all_patrons())
+
+    def test_get_all_patrons_multiple(self):
+        mock_patron = Mock(Patron)
+        mock_patron.get_memberid = Mock()
+
+        CuT = Library_DB()
+        CuT.close_db()
+        CuT.db = Mock()
+        CuT.db.all = Mock()
+        CuT.db.all.return_value = [mock_patron, mock_patron]  # list of patrons in db
+
+        self.assertEqual([mock_patron, mock_patron], CuT.get_all_patrons())
+
+    def test_update_patron_not_patron(self):
+        CuT = Library_DB()
+        CuT.close_db()
+        self.assertEqual(None, CuT.update_patron(0))
+
+    def test_update_patron(self):
+        mock_patron = Mock(Patron)
+        mock_patron.get_memberid = Mock()
+        CuT = Library_DB()
+        CuT.close_db()
+        CuT.db = Mock()
+        CuT.convert_patron_to_db_format = Mock()
+        CuT.convert_patron_to_db_format.return_value = mock_patron
+        CuT.db.update = Mock()
+
+        CuT.update_patron(mock_patron)
+        CuT.db.update.assert_called_once()
+
+    def test_retrieve_patron(self):
+        CuT = Library_DB()
+        CuT.close_db()
+        CuT.db = Mock()
+        CuT.db.search = Mock()
+        CuT.db.search.return_value = False
+        self.assertEqual(None, CuT.retrieve_patron(1))
+
+    def test_close_db(self):
+        CuT = Library_DB()
+        CuT.close_db()
+        CuT.db = Mock()
+        CuT.db.close = Mock()
+        CuT.close_db()
+        CuT.db.close.assert_called_once()
