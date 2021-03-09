46be68cb22255ed06af42c74d2928741bc10379e
ajs2290 <ajs2290@g.rit.edu>
2021-03-09 12:32:43 -0500

    Only one more function to test!

--- tests/test_api.py
+++ tests/test_api.py
@@ -43,3 +43,99 @@ class TestIsBookAvailable(unittest.TestCase):
             response = Books_API.is_book_available(self.books, 'Redwall')
             self.assertFalse(response)
 
+
+class TestGetBooksByAuthor(unittest.TestCase):
+    def setUp(self):
+        self.books = Books_API()
+
+    def test_valid_author_one_book(self):
+        with patch('library.ext_api_interface.Books_API.make_request') as mock_get:
+            mock_get.return_value = {'docs': [{'title_suggest': 'Redwall'}]}
+            response = Books_API.books_by_author(self.books, "Brian Jacques")
+            self.assertEqual(1, len(response))
+
+    def test_valid_author_three_books(self):
+        with patch('library.ext_api_interface.Books_API.make_request') as mock_get:
+            mock_get.return_value = {
+                'docs': [{'title_suggest': 'Redwall'}, {'title_suggest': 'Cornflower'}, {'title_suggest': 'Martin'}]}
+            response = Books_API.books_by_author(self.books, 'Brian Jacques')
+            self.assertEqual(3, len(response))
+
+    def test_valid_author_correct_title(self):
+        with patch('library.ext_api_interface.Books_API.make_request') as mock_get:
+            mock_get.return_value = {
+                'docs': [{'title_suggest': 'Redwall'}]}
+            response = Books_API.books_by_author(self.books, 'Brian Jacques')
+            self.assertEqual('Redwall', response[0])
+
+    def test_no_books_found_still_docs(self):
+        with patch('library.ext_api_interface.Books_API.make_request') as mock_get:
+            mock_get.return_value = {
+                'docs': []}
+            response = Books_API.books_by_author(self.books, "Brian Jacques")
+            self.assertEqual(0, len(response))
+
+    def test_no_books_found_no_data(self):
+        with patch('library.ext_api_interface.Books_API.make_request') as mock_get:
+            response = Books_API.books_by_author(self.books, "Brian Jacques")
+            self.assertEqual(0, len(response))
+
+
+class TestGetBookInfo(unittest.TestCase):
+    def setUp(self):
+        self.books = Books_API()
+
+    def test_get_book_info_all_data(self):
+        with patch('library.ext_api_interface.Books_API.make_request') as mock_get:
+            mock_get.return_value = {
+                'docs': [{'title': 'Redwall', 'publisher': 'Philomel', 'publish_year': 1986, 'language': 'English'}]}
+            expected_book = {'title': 'Redwall', 'publisher': 'Philomel', 'publish_year': 1986, 'language': 'English'}
+            response = Books_API.get_book_info(self.books, "Redwall")
+            self.assertEqual(expected_book, response[0])
+
+    def test_get_book_info_no_publisher(self):
+        with patch('library.ext_api_interface.Books_API.make_request') as mock_get:
+            mock_get.return_value = {
+                'docs': [{'title': 'Redwall','publish_year': 1986, 'language': 'English'}]}
+            expected_book = {'title': 'Redwall', 'publish_year': 1986, 'language': 'English'}
+            response = Books_API.get_book_info(self.books, "Redwall")
+            self.assertEqual(expected_book, response[0])
+
+    def test_get_book_info_no_year(self):
+        with patch('library.ext_api_interface.Books_API.make_request') as mock_get:
+            mock_get.return_value = {
+                'docs': [{'title': 'Redwall', 'publisher': 'Philomel', 'language': 'English'}]}
+            expected_book = {'title': 'Redwall', 'publisher': 'Philomel', 'language': 'English'}
+            response = Books_API.get_book_info(self.books, "Redwall")
+            self.assertEqual(expected_book, response[0])
+
+    def test_get_book_info_no_language(self):
+        with patch('library.ext_api_interface.Books_API.make_request') as mock_get:
+            mock_get.return_value = {
+                'docs': [{'title': 'Redwall', 'publisher': 'Philomel', 'publish_year': 1986}]}
+            expected_book = {'title': 'Redwall', 'publisher': 'Philomel', 'publish_year': 1986}
+            response = Books_API.get_book_info(self.books, "Redwall")
+            self.assertEqual(expected_book, response[0])
+
+    def test_get_book_info_minimal_data(self):
+        with patch('library.ext_api_interface.Books_API.make_request') as mock_get:
+            mock_get.return_value = {
+                'docs': [{'title': 'Redwall'}]}
+            expected_book = {'title': 'Redwall'}
+            response = Books_API.get_book_info(self.books, "Redwall")
+            self.assertEqual(expected_book, response[0])
+
+    def test_get_book_info_no_data_still_docs(self):
+        with patch('library.ext_api_interface.Books_API.make_request') as mock_get:
+            mock_get.return_value = {
+                'docs': []}
+            response = Books_API.get_book_info(self.books, "Redwall")
+            self.assertEqual(0, len(response))
+
+    def test_get_book_info_two_books(self):
+        with patch('library.ext_api_interface.Books_API.make_request') as mock_get:
+            mock_get.return_value = {
+                'docs': [{'title': 'Redwall', 'publisher': 'Philomel', 'publish_year': 1986, 'language': 'English'},{'title':'Cornflower'}]}
+            expected_books = [{'title': 'Redwall', 'publisher': 'Philomel', 'publish_year': 1986, 'language': 'English'},{'title':'Cornflower'}]
+            response = Books_API.get_book_info(self.books, "Redwall")
+            self.assertEqual(expected_books, response)
